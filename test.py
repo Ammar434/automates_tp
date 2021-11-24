f362b1289b085b95f667ac6bab4593e9aac04b49
ammar <raoulammar434@gmail.com>
2021-11-24 13:44:59 +0100

    automates_tp

---
+++ test.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Code modifiable.
+"""
+
+from automate import Automate
+from state import State
+from transition import Transition
+from myparser import *
+
+# Question 1
+s0 = State(0, True, False)
+s1 = State(1, False, False)
+s2 = State(2, False, True)
+t1 = Transition(s0, "a", s0)
+t2 = Transition(s0, "b", s1)
+t3 = Transition(s1, "a", s2)
+t4 = Transition(s1, "b", s2)
+t5 = Transition(s2, "a", s0)
+t6 = Transition(s2, "b", s1)
+liste = [t1, t2, t3, t4, t5, t6]
+auto = Automate(liste)
+print(auto)
+auto.show("auto")
+auto1 = Automate([t1, t2, t3, t4, t5, t6], [s0, s1, s2])
+print(auto1)
+auto1.show("auto1")
+auto2 = Automate.creationAutomate("auto.txt")
+print(auto2)
+auto2.show("auto2")
+# Question 2
+t = Transition(0, "a", 1)
+auto.removeTransition(t)
+auto.removeTransition(t1)
+print(auto)
+auto.addTransition(t1)
+
+auto.removeState(s1)
+print(auto)
+auto.addState(s1)
+
+print(auto1.getListTransitionsFrom(s1))
